01812da04894bf709dca46519fb15081639a2641
AttiqUrRehman99 <50966019+AttiqUrRehman99@users.noreply.github.com>
2020-06-23 00:22:31 +0500

    updated custom tags

--- src/Dinner.js
+++ src/Dinner.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Dinner(props) {
-  return <h2> We are serving {props.name} Today. </h2>;
+  return <Dish> We are serving {props.name} Today. </Dish>;
 }
 
 export default Dinner;
